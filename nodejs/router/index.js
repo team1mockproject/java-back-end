830dafbe20e4a4cbb94a416d4669083e1c208a72
Kiệt Kùn <ilovevy19@gmail.com>
2024-07-05 15:51:24 +0700

    Delete nodejs directory

--- nodejs/router/index.js
+++
@@ -1,6 +0,0 @@
-const useRouter = require('./userRouter');
-const routes =(app)=>{
-    app.use('/user',useRouter)
-    
-}
-module.exports = routes;
--- nodejs/router/userRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {createUser,loginUser} = require('../controller/userController');
-router.post('/', createUser)
-router.post('/login', loginUser)
-module.exports = router;

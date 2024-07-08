80b6660201d6f159b2c7876a373ced42a4321420
QuangVinh <bloodmoon.251103@gmail.com>
2024-07-09 05:57:58 +0700

    Create: Manager Sidebar UI

--- react/mock-project-frontend/src/components/manager-header/ManagerHeader.jsx
+++ react/mock-project-frontend/src/components/manager-header/ManagerHeader.jsx
@@ -1,43 +1,123 @@
-import { IoLogOutOutline, IoNotificationsOutline, IoNotificationsSharp } from "react-icons/io5"
+import { IoLogOutOutline, IoMenu, IoNotificationsOutline, IoNotificationsSharp } from "react-icons/io5"
 import Logo from "../../assets/logo-1.jfif"
 import { FaChevronDown, FaInfoCircle, FaUser } from "react-icons/fa"
 import { MdVpnKey } from "react-icons/md"
-import { useState } from "react"
-const ManagerHeader = () => {
+import { useContext, useState } from "react"
+import ResponsiveContext from "../client-context"
+const ManagerHeader = (props) => {
+    const { setIsShowSidebar } = props
     const [isHover, setIsHover] = useState(false)
+    const { isMobile, isTablet, isDesktop } = useContext(ResponsiveContext)
     return (
         <ul className="h-[70px] bg-[var(--color-primary)] flex justify-between items-center px-4">
-            <li>
-                <img
-                    className="h-[70px]"
-                    src={Logo}
-                    alt="" />
-            </li>
-            <li className="flex items-center text-white cursor-pointer h-full">
-                <div className="flex items-center mr-10 h-full" onMouseOver={() => { setIsHover(true) }} onMouseLeave={() => { setIsHover(false) }}>
-                    {isHover ? <IoNotificationsSharp className="text-2xl mr-2" /> : <IoNotificationsOutline className="text-2xl mr-2" />}
-                    <span>Notification</span>
-                </div>
-                <div className="group flex items-center relative cursor-pointer h-full">
-                    <FaUser className="text-2xl mr-2" />
-                    <span>Manager</span>
-                    <FaChevronDown className="ml-1" />
-                    <ul className="group-hover:block hidden absolute text-black top-full right-0 w-[250px] p-1 bg-white shadow-[0_9px_28px_8px_rgba(0,0,0,0.05)] rounded-md">
-                        <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
-                            <FaInfoCircle className="text-lg mr-2" />
-                            Personal information
-                        </li>
-                        <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
-                            <MdVpnKey className="text-lg mr-2" />
-                            Change password
-                        </li>
-                        <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
-                            <IoLogOutOutline className="text-lg mr-2" />
-                            Logout
-                        </li>
-                    </ul>
-                </div>
-            </li>
+            {isDesktop &&
+                <>
+                    <li>
+                        <img
+                            className="h-[70px]"
+                            src={Logo}
+                            alt="" />
+                    </li>
+                    <li className="flex items-center text-white cursor-pointer h-full">
+                        <div className="flex items-center mr-10 h-full" onMouseOver={() => { setIsHover(true) }} onMouseLeave={() => { setIsHover(false) }}>
+                            {isHover ? <IoNotificationsSharp className="text-2xl mr-2" /> : <IoNotificationsOutline className="text-2xl mr-2" />}
+                            <span>Notification</span>
+                        </div>
+                        <div className="group flex items-center relative cursor-pointer h-full">
+                            <FaUser className="text-2xl mr-2" />
+                            <span>Manager</span>
+                            <FaChevronDown className="ml-1" />
+                            <ul className="group-hover:block hidden absolute text-black top-full right-0 w-[250px] p-1 bg-white shadow-[0_9px_28px_8px_rgba(0,0,0,0.05)] rounded-md">
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <FaInfoCircle className="text-lg mr-2" />
+                                    Personal information
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <MdVpnKey className="text-lg mr-2" />
+                                    Change password
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <IoLogOutOutline className="text-lg mr-2" />
+                                    Logout
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </>
+            }
+            {isTablet &&
+                <>
+                    <li className="flex items-center">
+                        <img
+                            className="h-[70px]"
+                            src={Logo}
+                            alt="" />
+                        <IoMenu className="text-3xl text-white ml-5"
+                            onClick={() => { setIsShowSidebar(true) }}
+                        />
+                    </li>
+                    <li className="flex items-center text-white cursor-pointer h-full">
+                        <div className="flex items-center mr-10 h-full" onMouseOver={() => { setIsHover(true) }} onMouseLeave={() => { setIsHover(false) }}>
+                            {isHover ? <IoNotificationsSharp className="text-2xl mr-2" /> : <IoNotificationsOutline className="text-2xl mr-2" />}
+                        </div>
+                        <div className="group flex items-center relative cursor-pointer h-full">
+                            <FaUser className="text-2xl mr-2" />
+                            <span>Manager</span>
+                            <FaChevronDown className="ml-1" />
+                            <ul className="group-hover:block hidden absolute text-black top-full right-0 w-[250px] p-1 bg-white shadow-[0_9px_28px_8px_rgba(0,0,0,0.05)] rounded-md">
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <FaInfoCircle className="text-lg mr-2" />
+                                    Personal information
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <MdVpnKey className="text-lg mr-2" />
+                                    Change password
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <IoLogOutOutline className="text-lg mr-2" />
+                                    Logout
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </>
+            }
+            {isMobile &&
+                <>
+                    <li className="flex items-center">
+                        <img
+                            className="h-[70px]"
+                            src={Logo}
+                            alt="" />
+                        <IoMenu className="text-3xl text-white ml-1"
+                            onClick={() => { setIsShowSidebar(true) }}
+                        />
+                    </li>
+                    <li className="flex items-center text-white cursor-pointer h-full">
+                        <div className="flex items-center mr-3 h-full" onMouseOver={() => { setIsHover(true) }} onMouseLeave={() => { setIsHover(false) }}>
+                            {isHover ? <IoNotificationsSharp className="text-2xl mr-2" /> : <IoNotificationsOutline className="text-2xl mr-2" />}
+                        </div>
+                        <div className="group flex items-center relative cursor-pointer h-full">
+                            <FaUser className="text-2xl mr-2" />
+                            <FaChevronDown className="ml-1" />
+                            <ul className="group-hover:block hidden absolute text-black top-full right-0 w-[250px] p-1 bg-white shadow-[0_9px_28px_8px_rgba(0,0,0,0.05)] rounded-md">
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <FaInfoCircle className="text-lg mr-2" />
+                                    Personal information
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <MdVpnKey className="text-lg mr-2" />
+                                    Change password
+                                </li>
+                                <li className="flex items-center pl-4 py-3 hover:bg-gray-100 cursor-pointer rounded-md">
+                                    <IoLogOutOutline className="text-lg mr-2" />
+                                    Logout
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </>
+            }
         </ul>
     )
 }
--- react/mock-project-frontend/src/pages/Manager.jsx
+++ react/mock-project-frontend/src/pages/Manager.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import ManagerHeader from "../components/manager-header"
 import ManagerSidebar from "../components/manager-sidebar"
 const Manager = () => {
+    const [isShowSidebar, setIsShowSidebar] = useState(false)
     return (
         <>
-            <ManagerHeader />
+            <ManagerHeader setIsShowSidebar={setIsShowSidebar} />
             <div>
-                <ManagerSidebar />
+                <ManagerSidebar
+                    isShowSidebar={isShowSidebar}
+                    setIsShowSidebar={setIsShowSidebar}
+                />
             </div>
         </>
     )

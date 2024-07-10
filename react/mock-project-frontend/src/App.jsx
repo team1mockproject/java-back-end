8f713d02451aec073ce0cd5643d0324ab88dbb5e
LuanDHV <luandhv1406@gmail.com>
2024-07-10 08:25:17 +0700

    CREATE: Register To Auction

--- react/mock-project-frontend/src/App.jsx
+++ react/mock-project-frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import './styles/global.css'
-import ClientHome from './pages/client-home'
-import Client from './pages/Client'
-import Login from './pages/login'
-import Signup from './pages/signup'
-import Manager from './pages/Manager'
-import { ResponsiveWrapper } from './context/responsive-context/ResponsiveContext'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import "./styles/global.css";
+import ClientHome from "./pages/client-home";
+import Client from "./pages/Client";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Manager from "./pages/Manager";
+import { ResponsiveWrapper } from "./context/responsive-context/ResponsiveContext";
+import ClientRegisterToAuction from "./pages/client-register-to-auction";
 
 const router = createBrowserRouter([
   {
@@ -16,32 +17,34 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <ClientHome />
-      }
-    ]
+        element: <ClientHome />,
+      },
+      {
+        path: "/register-to-auction",
+        element: <ClientRegisterToAuction />,
+      },
+    ],
   },
   {
     path: "/manager",
-    element: <Manager />
+    element: <Manager />,
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "/signup",
-    element: <Signup />
-  }
-])
+    element: <Signup />,
+  },
+]);
 
 function App() {
   return (
     <ResponsiveWrapper>
-      <RouterProvider
-        router={router}
-      />
+      <RouterProvider router={router} />
     </ResponsiveWrapper>
-  )
+  );
 }
 
-export default App
+export default App;

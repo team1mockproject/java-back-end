8cfdb639f425faecd650534e923e235da7ba2ec9
LuanDHV <luandhv1406@gmail.com>
2024-07-15 04:17:04 +0700

    client auction & manager auction

--- react/mock-project-frontend/src/App.jsx
+++ react/mock-project-frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import './styles/global.css'
-import ClientHome from './pages/client-home'
-import Client from './pages/Client'
-import Login from './pages/login'
-import Signup from './pages/signup'
-import Manager from './pages/Manager'
-import { ResponsiveWrapper } from './context/responsive-context/ResponsiveContext'
-import ManagerBuyerList from './pages/manager-buyer-list'
-import ManagerSellerList from './pages/manager-seller-list'
-import ManagerAppraiserList from './pages/manager-appraiser-list'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import "./styles/global.css";
+import ClientHome from "./pages/client-home";
+import Client from "./pages/Client";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Manager from "./pages/Manager";
+import { ResponsiveWrapper } from "./context/responsive-context/ResponsiveContext";
+import ManagerBuyerList from "./pages/manager-buyer-list";
+import ManagerSellerList from "./pages/manager-seller-list";
+import ManagerAppraiserList from "./pages/manager-appraiser-list";
 import ClientRegisterToAuction from "./pages/client-register-to-auction";
+import ManagerAuctionList from "./pages/manager-auction-list";
+import ClientListAuction from "./pages/client-list-auction";
 
 const router = createBrowserRouter([
   {
@@ -23,8 +25,12 @@ const router = createBrowserRouter([
         element: <ClientHome />,
       },
       {
-        path: "/register-to-auction",
+        path: "register-to-auction",
         element: <ClientRegisterToAuction />,
+      },
+      {
+        path: "list-auction",
+        element: <ClientListAuction />,
       },
     ],
   },
@@ -35,17 +41,21 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <ManagerBuyerList />
+        element: <ManagerBuyerList />,
       },
       {
         path: "seller",
-        element: <ManagerSellerList />
+        element: <ManagerSellerList />,
       },
       {
         path: "appraiser",
-        element: <ManagerAppraiserList />
-      }
-    ]
+        element: <ManagerAppraiserList />,
+      },
+      {
+        path: "auction",
+        element: <ManagerAuctionList />,
+      },
+    ],
   },
   {
     path: "/login",

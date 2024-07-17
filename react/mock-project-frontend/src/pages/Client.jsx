94cebdf147debec794b06e2638574f02b9586387
NghiaVTS <nyclone569@gmail.com>
2024-07-17 09:15:44 +0700

    Update Client.jsx

--- react/mock-project-frontend/src/pages/Client.jsx
+++ react/mock-project-frontend/src/pages/Client.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom"
 import ClientHeader from "../components/client-header"
-import ClientFooter from "../components/client-footer/ClientFooter"
 
 const Client = () => {
     return (
         <>
             <ClientHeader />
             <Outlet />
-            <ClientFooter />
         </>
     )
 }

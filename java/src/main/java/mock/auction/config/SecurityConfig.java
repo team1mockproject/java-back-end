0d7d2ec6f0b5d7afd831ce4aec94cbc79412b332
kiet-kun-afk <ilovevy19@gmail.com>
2024-07-22 15:28:23 +0700

    fix mapping

--- java/src/main/java/mock/auction/config/SecurityConfig.java
+++ java/src/main/java/mock/auction/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package mock.auction.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import mock.auction.security.JwtRequestFilter;
 
@@ -14,11 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -29,19 +23,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("*"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }))
                 .csrf(csrf -> csrf.disable())
                 .addFilterBefore(jwtRequestFilter, BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(
--- java/src/main/java/mock/auction/controller/AccountAPI.java
+++ java/src/main/java/mock/auction/controller/AccountAPI.java
@@ -13,7 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/account/")
+@RequestMapping("/api/authenticate/account/")
 public class AccountAPI extends BaseAPI<AccountDto, AccountEntity> {
     private AccountServiceImpl accountService;
 
